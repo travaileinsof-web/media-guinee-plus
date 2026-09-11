7f70d9266e9813ed05a531028f058fdac975e10f
Billy Doumbouya <192708154+Clinton62395@users.noreply.github.com>
2026-09-11 02:06:44 +0000

    fix(backend): Return actual error message in global error handler

--- server.ts
+++ server.ts
@@ -720,7 +720,7 @@ app.use(
   ) => {
     console.error(error);
     if (res.headersSent) return next(error);
-    res.status(400).json({ error: "La requête n’a pas pu être traitée." });
+    res.status(400).json({ error: error.message || "La requête n’a pas pu être traitée." });
   },
 );
 
